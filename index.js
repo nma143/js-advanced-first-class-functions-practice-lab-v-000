7175c2e2f904aa0a8c5d64627ed9bbe0929fe908
Nikki A <nikki.apostolakis@gmail.com>
2019-04-08 17:43:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,5 @@ function logDriversByHometown(drivers, location)
 }
 function driversByRevenue(drivers)
 {
-  drivers.sort(function compareRevenu(driver1, driver2)
-  {
-    return driver1.revenue - driver2.revenue;
-  });
+
 }
